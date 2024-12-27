da9465c8a9babe9f2bff499d70667096fa8ea599
FabrizioDadamo <154342880+FabrizioDadamo@users.noreply.github.com>
2024-12-27 15:56:24 +0100

    Update with install requirements

--- energy_management/__init__.py
+++ energy_management/__init__.py
@@ -1,1 +1,15 @@
 from . import models
+import os
+import subprocess
+
+# Funzione per installare automaticamente le dipendenze
+def install_requirements():
+    requirements_file = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+    if os.path.exists(requirements_file):
+        try:
+            subprocess.check_call(['pip', 'install', '-r', requirements_file])
+        except Exception as e:
+            raise ImportError(f"Errore durante l'installazione delle dipendenze: {str(e)}")
+
+# Esegui l'installazione
+install_requirements()
